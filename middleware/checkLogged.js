d1cf3987d432059354756a64f5e4267f3021024d
kaw0n <130693205+kaw0n@users.noreply.github.com>
2024-01-10 19:21:01 +0100

    ostateczne

---
+++ middleware/checkLogged.js
@@ -0,0 +1,10 @@
+module.exports = (req, res, next) => {
+    const token = req.cookies.authToken;
+  // Check if the user is authenticated
+  if (token) {
+    return res.status(400).send('<script>alert("Jesteś już zalogowany"); window.location.href = "/post";</script>');
+  }
+
+  // If not authenticated, continue to the next middleware
+  return next();
+};
--- router/userRoutes.js
+++ router/userRoutes.js
@@ -3,12 +3,13 @@ const multer = require('multer');
 const userControllers = require('../controllers/userControllers');
 const router = express.Router(); 
 const checkAuth = require("../middleware/chechAuth");
+const checkLogged = require("../middleware/checkLogged")
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.get('/register/create',userControllers.user_create_view);
-router.post('/register/login', userControllers.log_in_method);
+router.get('/register/create', checkLogged, userControllers.user_create_view);
+router.post('/register/login', checkLogged, userControllers.log_in_method);
 router.post('/register', userControllers.create_user);
 router.get('/login',userControllers.user_login_view);
 router.post('/logout',checkAuth, userControllers.logout);
